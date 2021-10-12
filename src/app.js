ec47dbfcf55106f82f986c8438116da9290421cf
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-10-12 15:14:05 +0200

    install mysql2 + sequelize

--- src/app.js
+++ src/app.js
@@ -2,21 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const homeRouter = require('./routes/home')
-const mysql = require('mysql');
-require('dotenv').config()
-
-const connection = mysql.createConnection({
-    connectionLimit : 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-});
 
 app.use(express.json())
 app.use(homeRouter)
