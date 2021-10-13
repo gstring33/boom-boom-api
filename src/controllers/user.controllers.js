740c5add899c73e485608670591e1102edc461c2
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-10-13 10:23:37 +0200

    create endpoint to create new user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,8 +3,39 @@ const User = db.user;
 const Op = db.Sequelize.Op;
 
 // Create and Save a new User
+// Method:POST, Endpoint:/user
 exports.create = (req, res) => {
+    // Validate request
+    if (!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+        return;
+    }
 
+    // Create a Tutorial
+    const user = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: req.body.email,
+        roles: req.body.roles,
+        is_active: req.body.is_active,
+        allowed_to_choose: 0,
+        last_connect: req.body.last_connect
+    };
+
+    // Save User in the database
+    User.create(user)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Tutorial."
+            });
+        });
 };
 
 // Retrieve all Users from the database.
