2a7fa38a33b58e2d39a28547172f29c8e7150580
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-10-13 09:56:10 +0200

    implement user controller

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,28 @@
+const db = require("../models");
+const User = db.user;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new User
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all Users from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single User with an id
+exports.findOneById = (req, res) => {
+
+};
+
+// Update a User by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a User with the specified id in the request
+exports.deleteOne = (req, res) => {
+
+};
