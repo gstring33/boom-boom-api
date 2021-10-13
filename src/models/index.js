c3ff827ce080ea2a680b246e8a9b323f44da0fc4
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-10-13 10:22:48 +0200

    convert pool params es integer

--- src/models/index.js
+++ src/models/index.js
@@ -5,8 +5,8 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     dialect: process.env.DB_DIALECT,
     operatorsAliases: false,
     pool: {
-        max: process.env.DB_POOL_MAX,
-        min: process.env.DB_POOL_MIN,
+        max: parseInt(process.env.DB_POOL_MAX),
+        min: parseInt(process.env.DB_POOL_MIN),
         acquire: process.env.DB_POOL_ACQUIRE,
         idle: process.env.DB_POOL_IDLE
     }
