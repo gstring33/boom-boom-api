be997848e0e7f18aa5a59fcdfaf2539e24dedc50
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-10-14 11:03:36 +0200

    Install Crypto

--- src/config/security.config.js
+++ src/config/security.config.js
@@ -1,3 +1,9 @@
+require('dotenv').config()
+
 module.exports = {
-    roles: ['ROLE_ADMIN', 'ROLE_USER']
+    roles: ['ROLE_ADMIN', 'ROLE_USER'],
+    crypto: {
+        secret: process.env.SECRET_KEY,
+        algorithm: 'sha256'
+    }
 }
