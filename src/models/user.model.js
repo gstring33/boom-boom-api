7973e764141aa17367756f99e3ff766fa1316708
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-10-14 10:13:15 +0200

    Add constraint to User model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,28 +1,40 @@
 module.exports = (sequelize, Sequelize) => {
     const User = sequelize.define("user", {
         firstname: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
         },
         lastname: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
         },
         email: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
         },
         password: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
         },
         roles: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false,
+            defaultValue: 'ROLE_USER'
         },
         is_active: {
-            type: Sequelize.INTEGER
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            defaultValue: 0
         },
         allowed_to_choose: {
-            type: Sequelize.INTEGER
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            defaultValue: 0
         },
         last_connect: {
-            type: Sequelize.DATE
+            type: Sequelize.DATE,
+            allowNull: true
         },
     });
 
