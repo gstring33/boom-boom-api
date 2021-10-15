f5b6fa8ccef9fcfb04a271bcfe3833d5794ccfca
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-10-15 16:42:52 +0200

    get database env variables in security config file

--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,21 @@
 require('dotenv').config()
 const sequelizeConfig = require('../config/sequelize.config')
+const securityConfig = require('../config/security.config')
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: sequelizeConfig.dialect,
-    operatorsAliases: sequelizeConfig.operatorsAliases,
-    pool: {
-        max: sequelizeConfig.pool.max,
-        min: sequelizeConfig.pool.min,
-        acquire: sequelizeConfig.pool.acquire,
-        idle: sequelizeConfig.pool.idle
-    }
-});
+const sequelize = new Sequelize(
+    securityConfig.database.name,
+    securityConfig.database.user,
+    securityConfig.database.password, {
+        host: securityConfig.database.host  ,
+        dialect: sequelizeConfig.dialect,
+        operatorsAliases: sequelizeConfig.operatorsAliases,
+        pool: {
+            max: sequelizeConfig.pool.max,
+            min: sequelizeConfig.pool.min,
+            acquire: sequelizeConfig.pool.acquire,
+            idle: sequelizeConfig.pool.idle
+        }
+    });
 
 const db = {};
 
