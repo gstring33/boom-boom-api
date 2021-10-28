01e0656ad4c752ad5f1c7fdf8ee89a40ec0db8e6
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-10-28 17:21:55 +0200

    load dynamically model classes

--- db/models/index.js
+++ db/models/index.js
@@ -1,21 +1,21 @@
 'use strict';
 
-//const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-//const basename = path.basename(__filename);
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const config = require('../../config/database.config')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config.sequelize);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config.sequelize);
+  sequelize = new Sequelize(config.database, config.username, config.password, {host: config.host, dialect: config.dialect});
 }
 
-/*fs
+fs
   .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
@@ -29,18 +29,9 @@ Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
-});*/
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-const user = require("./user.model.js")(sequelize, Sequelize)
-const event = require("./event.model.js")(sequelize, Sequelize)
-
-user.hasMany(event)
-event.belongsTo(user)
-
-db.user = user;
-db.event = event;
 
 module.exports = db;
